087ad08c49b8c148aaa3547facff7dc9a81d48ec
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2024-08-15 21:49:26 -0300

    Teste API POST
    Construção e teste de api usando POST

--- src/test/java/apiTeste/testeUser.java
+++ src/test/java/apiTeste/testeUser.java
@@ -5,13 +5,45 @@ package apiTeste;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is; //comparador se é igual
+
 //Classe
 public class testeUser {   //inicio da classe
     //atributos
 
     //funções e metodos
-    @Test
-    public void testarIncluirUser(){
 
+    //funções de apoio
+    public static String lerArquivoJson(String arquivoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+    }
+
+    //funções de teste
+
+    @Test
+    public void testarIncluirUser() throws IOException {
+        //carregar dados do json
+        String jsonBody = lerArquivoJson("src/test/resources/json/user1.json");
+        String userId = "1373879393";
+
+        //realizar teste
+        given()                                         //Dado que
+                .contentType("application/json")     //o tipo de conteudo
+                .log().all()                            //mostre tudo
+                .body(jsonBody)                         //corpo da requisição
+                .when()                                         //Quando
+                .post("https://petstore.swagger.io/v2/user") //endpoint
+                .then()                                          //então
+                .log().all()                             //mostre tudo na volta
+                .statusCode(200)                      //comunicação ida e volta ok
+                .body("code", is(200))          //tag code é 200
+                .body("type", is("unknown"))    //tag type é unknown
+                .body("message", is(userId))          //message é variável userId
+        ;
     }
 }// fim da classe
