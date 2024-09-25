50cd23cb919cfb9b67c4cee4f4b7ea1b925e1d6f
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2024-09-25 20:58:28 -0300

    cadastro

---
+++ src/main/java/praticandoMais/Cadastro.java
@@ -0,0 +1,24 @@
+package praticandoMais;
+
+import java.util.Scanner;
+
+public class Cadastro {
+
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        String cadastrado = nome + sobrenome;
+
+        String cadastrado1 = cadastrado.toUpperCase();
+
+        System.out.println("Nome: " + cadastrado1);
+
+        scanner.close();
+    }
+}
